54594fd68a4a979c7e756f552ed7920c0f7c2917
EricklisCruz <erickliscruz@gmail.com>
2022-12-28 14:23:56 -0300

    MÉTODO SORT E PAGEREQUEST

--- mudi/src/main/java/com/alura/mudi/mudi/controller/HomeController.java
+++ mudi/src/main/java/com/alura/mudi/mudi/controller/HomeController.java
@@ -4,6 +4,8 @@ import com.alura.mudi.mudi.model.Pedido;
 import com.alura.mudi.mudi.model.StatusPedido;
 import com.alura.mudi.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +22,9 @@ public class HomeController {
 
     @GetMapping
     private String home(Model model) {
-        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+        Sort sort = Sort.by("deliveryDate").descending(); //ORDENAÇÃO DA DATA MAIS ATUAL PARA A ANTIGA
+        PageRequest paginacao = PageRequest.of(0,10, sort);
+        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
         model.addAttribute("pedidos",pedidos);
         return "home";
     }
